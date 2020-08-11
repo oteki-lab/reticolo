d20198b8b248bb0e8066e8e9c18e31c4710a5887
oteki-lab <oteki@g.ecc.u-tokyo.ac.jp>
2020-08-11 19:39:57 +0900

    add notification function

---
+++ Codes/sendMail.m
@@ -0,0 +1,35 @@
+% ===== Info =====
+% Function for Send Mail
+% URL: https://www.umi-mori.jp/article/matlab/send_mail.php
+% Github: https://github.com/Masumi-M/SendMail_matlab
+
+%body = "reticolo Simulation Done.";
+%sendMail(body);
+
+% ===== Main =====
+function [] = sendMail(body,attachmentFilePath)
+    % Modify these two lines to reflect your account and password.
+    email = 'okadalab.matlab.notification@gmail.com';
+    password = 'Xw8fPKh4';
+    recipients = 'oteki@g.ecc.u-tokyo.ac.jp';
+
+    setpref('Internet', 'E_mail', email);
+    setpref('Internet', 'SMTP_Server', 'smtp.gmail.com');
+    setpref('Internet', 'SMTP_Username', email);
+    setpref('Internet', 'SMTP_Password', password);
+
+    props = java.lang.System.getProperties;
+    props.setProperty('mail.smtp.auth','true');
+    props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+    props.setProperty('mail.smtp.socketFactory.port','465');  %Socket for SSL (465)
+
+    subject = 'Matlab Notification.';
+    if attachmentFilePath == ""
+        sendmail(recipients, subject, body);
+    else
+        sendmail(recipients, subject, body, {char(attachmentFilePath)});
+    end
+
+    return
+
+end
