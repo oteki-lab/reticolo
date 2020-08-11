4b4eea1c8ceb1fc1464f966c9923f0026f4f463d
oteki-lab <oteki@g.ecc.u-tokyo.ac.jp>
2020-08-11 20:10:19 +0900

    fixed notification

--- reticolo_eng.m
+++ reticolo_eng.m
@@ -583,7 +583,7 @@ try
         ylabel('z')
         hold off
 
-        filename = append("results\", "cross-section", ".png");
+        filename = append("results\", "cross-section ", datestr(datetime('now'),'yyyyMMddHHmmssFFF'), ".png");
         saveas(gcf, filename);
     end
 
@@ -627,7 +627,7 @@ try
         set(gcf,'color','w');
         box on
 
-        filename = append("results\", "absorption graph", ".png");
+        filename = append("results\", "absorption graph ", datestr(datetime('now'),'yyyyMMddHHmmssFFF'), ".png");
         saveas(gcf, filename);
     end
 
